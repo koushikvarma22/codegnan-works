df905b1f46342bc080b07f144c75d8e028ff425f
koushikvarma_jampana <koushikvarma22@gmail.com>
2026-04-15 10:47:14 +0530

    Add files via upload
    day 1 of my learning in codegnan

---
+++ day1.py.py
@@ -0,0 +1,22 @@
+print("This is my first day at codegnan")
+'''
+what is python?
+-->python is a high_level language means no need to worry about low-level mangement like memory allocation
+-->python never saves anything perminantly,it will only save at run time
+-->python supports object oriented programming
+print(id(a))
+c=int(input("enter a number"))
+d=int(input("enter a number"))
+print(a+b)
+print(f"b={b}")
+print(f"a={a}")
+-->python is dynamically typed language means no need to mention the type of data your take in varible.
+a=9
+b="python"
+print(type(a))
+print(type(b))
+-->this is also called as interpreted language means it executes line by line i.e it  will not move to another line if a error in previous line.
+-->this is syntax less language.
+a=90
+print(a)
+'''
